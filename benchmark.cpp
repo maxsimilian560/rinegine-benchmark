669fa1f7f12f205a1bf525e7e271ce59374190b7
Максимилиан <maxsimilian560@gmail.com>
2026-08-31 15:58:49 +0300

    Fix

--- benchmark.cpp
+++ benchmark.cpp
@@ -12,20 +12,20 @@
 #include <cstddef>
 #include <new>
 
-// void* operator new[](std::size_t size, const char*, int, unsigned int, const char*, int) {
-//   return ::operator new[](size);
-//   // return Rinegine::Kernel::Allocator::GetDefault().allocate(size);
-// }
+void* operator new[](std::size_t size, const char*, int, unsigned int, const char*, int) {
+  return ::operator new[](size);
+  // return Rinegine::Kernel::Allocator::GetDefault().allocate(size);
+}
 
-// void* operator new(std::size_t size, const char*, int, unsigned int, const char*, int) {
-//   return ::operator new(size);
-//   // return Rinegine::Kernel::Allocator::GetDefault().allocate(size);
-// }
+void* operator new(std::size_t size, const char*, int, unsigned int, const char*, int) {
+  return ::operator new(size);
+  // return Rinegine::Kernel::Allocator::GetDefault().allocate(size);
+}
 
-// void operator delete[](void* ptr, const char*, int, unsigned int, const char*, int) noexcept {
-//   ::operator delete[](ptr);
-//   // Rinegine::Kernel::Allocator::GetDefault().deallocate(ptr);
-// }
+void operator delete[](void* ptr, const char*, int, unsigned int, const char*, int) noexcept {
+  ::operator delete[](ptr);
+  // Rinegine::Kernel::Allocator::GetDefault().deallocate(ptr);
+}
 // void operator delete(void* ptr, const char*, int, unsigned int, const char*, int) noexcept {
 //   // return ::operator new(size);
 //   Rinegine::Kernel::Allocator::GetDefault().deallocate(ptr);
